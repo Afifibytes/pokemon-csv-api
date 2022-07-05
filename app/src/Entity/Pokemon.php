540dda246022d401e73a3a7bf2b8da645d30e237
Mohamad Afifi <mafifi@192.168.1.4>
2022-07-05 06:41:02 +0200

    MODIFY pokemon add setId method

--- app/src/Entity/Pokemon.php
+++ app/src/Entity/Pokemon.php
@@ -48,6 +48,13 @@ class Pokemon
     #[ORM\Column(type: 'boolean')]
     private $legendary;
 
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
